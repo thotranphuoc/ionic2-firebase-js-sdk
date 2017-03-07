2cf85fb2e34fc673ed775918b0c8aa887cbf5f14
thotranphuoc <thotranphuoc@outlook.com>
2017-03-07 20:35:01 +0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,10 @@ import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 
 const firebaseConfig = {
-  const firebaseConfig = {
     apiKey: "YOUR_APP_KEY",
     authDomain: "YOUR_APP_DOMAIN.firebaseapp.com",
     databaseURL: "https://YOUR_APP_DOMAIN.firebaseio.com",
     storageBucket: "YOUR_APP_DOMAIN.appspot.com"
-  };
 };
 
 firebase.initializeApp(firebaseConfig);
